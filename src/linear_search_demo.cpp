735b8d7549261365074e6fddbdf1fc916f7ee49b
Jagadeesh Kotra <jagadeesh@stdin.top>
2021-07-28 19:34:51 +0530

    improve demos

--- src/linear_search_demo.cpp
+++ src/linear_search_demo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <assert.h>
+#include<thread>
 #include "../include/xcard.hpp"
 
 int main()
@@ -8,6 +9,7 @@ int main()
 
     std::string prefix_input;
     std::string _dbg_option;
+    int n_threads;
 
     std::cout << "Enter Prefix (Upto 15 digits): ";
     std::cin >> prefix_input;
@@ -16,13 +18,14 @@ int main()
     std::cout << "Enable Debug Messages [Y/N]: ";
     std::cin >> _dbg_option;
 
-    xcard xc(prefix_input, _dbg_option == "Y" ? true : false);
-    std::cout << xc.VERSION << std::endl;
 
-    for (size_t i = 0; i < 10; i++)
-    {
-        std::cout << xc.generateCard() << std::endl; 
-    }
+    std::cout << "No of threads to use (Max: " << std::thread::hardware_concurrency() << "): ";
+    std::cin >> n_threads;
+    assert(n_threads <= std::thread::hardware_concurrency());
+
+    xcardMT xcmt(prefix_input, n_threads, _dbg_option == "Y" ? true : false);
+    xcmt.LinearSearch();
 
     return 0;
+
 }
